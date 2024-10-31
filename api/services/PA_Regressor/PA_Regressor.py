812de089f6c08d175880c8d88bae2f60d46ccbfa
Luis Ramos <113364516+kekewicho@users.noreply.github.com>
2024-10-31 01:25:42 +0000

    ADD: Codigo de importacion de modelo desde google cloud storage. Aun comentado, resta probar

--- api/services/PA_Regressor/PA_Regressor.py
+++ api/services/PA_Regressor/PA_Regressor.py
@@ -1,5 +1,4 @@
 import joblib
-from typing import Union
 
 
 model = joblib.load('services/PA_Regressor/random_forest.pkl')
@@ -7,3 +6,35 @@ model = joblib.load('services/PA_Regressor/random_forest.pkl')
 
 def determinar_personal(tiempo, tpu, cantidad) -> float:
     return model.predict([[tiempo, tpu, cantidad]])[0]
+
+
+
+# import joblib
+# from google.cloud import storage
+
+# def cargar_modelo_desde_gcs(bucket_name, blob_name):
+#     """Carga un modelo desde un bucket de Google Cloud Storage.
+
+#     Args:
+#         bucket_name: Nombre del bucket de Google Cloud Storage.
+#         blob_name: Nombre del blob (archivo) dentro del bucket.
+
+#     Returns:
+#         El modelo cargado.
+#     """
+
+#     storage_client = storage.Client()
+#     bucket = storage_client.bucket(bucket_name)
+#     blob = bucket.blob(blob_name)
+#     blob.download_to_filename('temp_model.pkl')  # Descarga temporalmente el modelo
+#     model = joblib.load('temp_model.pkl')  # Carga el modelo desde el archivo temporal
+#     return model
+
+# # Reemplaza con los nombres de tu bucket y blob
+# bucket_name = 'tu_bucket_name'
+# blob_name = 'modelos/random_forest.pkl'
+
+# model = cargar_modelo_desde_gcs(bucket_name, blob_name)
+
+# def determinar_personal(tiempo, tpu, cantidad) -> float:
+#     return model.predict([[tiempo, tpu, cantidad]])[0]
